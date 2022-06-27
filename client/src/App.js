9ed599f73bf468495746fe4b7df977c7b310124d
Sanjaya Paudel <paudelsanjaya702@gmail.com>
2022-06-27 22:48:33 +0545

    frontend git issue solved

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import { React } from "react";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Nav from "./components/navbar/Nav";
+import Home from "./components/home/Home";
+import Auth from "./components/auth/Auth";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Container maxWidth="lg">
+          <Nav />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/auth" exact component={Auth} />
+          </Switch>
+        </Container>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
